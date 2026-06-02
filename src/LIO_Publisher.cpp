48e456678592bf52a84ef041075f13385980175e
lee <lizw_0304@163.com>
2026-06-02 11:50:56 +0800

    修改了上面的BUG

--- src/LIO_Publisher.cpp
+++ src/LIO_Publisher.cpp
@@ -1,13 +1,23 @@
 #include "ros/ros.h"
 #include "ros/publisher.h"
 #include "ros/subscriber.h"
+
 #include "nav_msgs/Odometry.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "std_msgs/Bool.h"
+
 #include <tf/transform_broadcaster.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <std_msgs/Bool.h>
+#include <tf/transform_datatypes.h>
+
 #include <math.h>
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <unistd.h>
+
+// Eigen
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
 
 // PCL
 #include <pcl/io/pcd_io.h>
@@ -18,6 +28,7 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
 #include <pcl/registration/icp.h>
 #include <pcl/filters/filter.h>
 #include <pcl/filters/voxel_grid.h>
@@ -37,22 +48,33 @@ public:
   ~LIO_Pub();
 
 private:
-  double dis_th;
+  double dis_th = 1.5;
+
   int signal_num = 0;
+
   bool isFirstOdom = true;
   bool Signal = false;
-  float distance = 0;
-  float x0, y0, z0;
-  float x1, y1, z1;
-  string NameSpace;
-  string RobotID;
-  string SensorName;
 
-  string tp_sub_pcd;
-  string tp_sub_odom;
+  float distance = 0.0;
+  float x0 = 0.0;
+  float y0 = 0.0;
+  float z0 = 0.0;
+  float x1 = 0.0;
+  float y1 = 0.0;
+  float z1 = 0.0;
+
+  std::string NameSpace;
+  std::string SensorName;
+
+  // 这里改成 int，不再用 string
+  int RobotID = 0;
+
+  std::string tp_sub_pcd;
+  std::string tp_sub_odom;
 
   ros::Subscriber odometrySubscriber_;
   ros::Subscriber pointCloudSubscriber_;
+
   ros::Publisher pointCloudPublisher_;
   ros::Publisher signalPublisher_;
   ros::Publisher subMapPublisher_;
@@ -61,9 +83,11 @@ private:
 
   void OdomCallback(nav_msgs::Odometry msg);
   void PCCallback(sensor_msgs::PointCloud2 msg);
+
   void pub_Signal();
   void pub_PC(sensor_msgs::PointCloud2 msg);
   void pub_TF(nav_msgs::Odometry msg);
+
   void reformatRobotName();
   Eigen::Isometry3d odom2isometry(const nav_msgs::Odometry odom_msg);
 };
@@ -76,18 +100,32 @@ LIO_Pub::~LIO_Pub()
 LIO_Pub::LIO_Pub(ros::NodeHandle &n)
 {
   // get the params
-  n.getParam("dis_th", dis_th);
-  n.getParam("NameSpace", NameSpace);
-  n.getParam("RobotID", RobotID);
-  n.getParam("SensorName", SensorName);
+  n.param<double>("dis_th", dis_th, 1.5);
+  n.param<std::string>("NameSpace", NameSpace, "vehicle0");
+  n.param<int>("RobotID", RobotID, 0);
+  n.param<std::string>("SensorName", SensorName, "velodyne");
 
-  n.param<string>("tp_sub_pcd", tp_sub_pcd, "cloud_registered");
-  n.param<string>("tp_sub_odom", tp_sub_odom, "Odometry");
+  n.param<std::string>("tp_sub_pcd", tp_sub_pcd, "cloud_registered");
+  n.param<std::string>("tp_sub_odom", tp_sub_odom, "Odometry");
+
+  // 防止异常 robot id
+  if (RobotID < 0)
+  {
+    ROS_WARN("RobotID is negative: %d, reset to 0", RobotID);
+    RobotID = 0;
+  }
+
+  if (RobotID > 255)
+  {
+    ROS_WARN("RobotID is larger than 255: %d, reset to 255", RobotID);
+    RobotID = 255;
+  }
 
   ROS_INFO("Robot_Namespace: %s", NameSpace.c_str());
+  ROS_INFO("RobotID: %d", RobotID);
+  ROS_INFO("SensorName: %s", SensorName.c_str());
   ROS_INFO("Subscribe pointcloud topic: %s", tp_sub_pcd.c_str());
   ROS_INFO("Subscribe odometry topic: %s", tp_sub_odom.c_str());
-  
   ROS_INFO("Get param dis_th = %lf", dis_th);
 
   reformatRobotName();
@@ -95,28 +133,42 @@ LIO_Pub::LIO_Pub(ros::NodeHandle &n)
   registeredCloud.reset(new pcl::PointCloud<pcl::PointXYZI>);
 
   // Subscriber
-  // odometrySubscriber_ = n.subscribe(NameSpace + "/Odometry", 1, &LIO_Pub::OdomCallback, this);
-  // pointCloudSubscriber_ = n.subscribe(NameSpace + "/cloud_registered", 1, &LIO_Pub::PCCallback, this);
-  odometrySubscriber_ = n.subscribe(NameSpace + "/" + tp_sub_odom, 1, &LIO_Pub::OdomCallback, this);
-  pointCloudSubscriber_ = n.subscribe(NameSpace + "/" + tp_sub_pcd, 1, &LIO_Pub::PCCallback, this);
+  odometrySubscriber_ =
+      n.subscribe(NameSpace + "/" + tp_sub_odom, 1, &LIO_Pub::OdomCallback, this);
+
+  pointCloudSubscriber_ =
+      n.subscribe(NameSpace + "/" + tp_sub_pcd, 1, &LIO_Pub::PCCallback, this);
 
   // Publisher
-  pointCloudPublisher_ = n.advertise<sensor_msgs::PointCloud2>(NameSpace + "/merged_cloud_registered", 5);
-  signalPublisher_ = n.advertise<std_msgs::Bool>(NameSpace + "/new_keyframe", 5);
-  subMapPublisher_ = n.advertise<dislam_msgs::SubMap>(NameSpace + "/submap", 5);
+  pointCloudPublisher_ =
+      n.advertise<sensor_msgs::PointCloud2>(NameSpace + "/merged_cloud_registered", 5);
+
+  signalPublisher_ =
+      n.advertise<std_msgs::Bool>(NameSpace + "/new_keyframe", 5);
+
+  subMapPublisher_ =
+      n.advertise<dislam_msgs::SubMap>(NameSpace + "/submap", 5);
 }
 
 // reformat robot name
 void LIO_Pub::reformatRobotName()
 {
   ROS_INFO("Check Format");
-  // check format
-  std::string slash = "/";
-  if ((NameSpace.find(slash)) == string::npos && !NameSpace.empty())
-    NameSpace = "/" + NameSpace;
-  if ((SensorName.find(slash)) == string::npos && !SensorName.empty())
-    SensorName = "/" + SensorName;
 
+  std::string slash = "/";
+
+  if ((NameSpace.find(slash)) == string::npos && !NameSpace.empty())
+  {
+    NameSpace = "/" + NameSpace;
+  }
+
+  if ((SensorName.find(slash)) == string::npos && !SensorName.empty())
+  {
+    SensorName = "/" + SensorName;
+  }
+
+  ROS_INFO("Formatted NameSpace: %s", NameSpace.c_str());
+  ROS_INFO("Formatted SensorName: %s", SensorName.c_str());
   ROS_INFO("Check Format Done");
 }
 
@@ -135,9 +187,10 @@ void LIO_Pub::OdomCallback(nav_msgs::Odometry msg)
   z1 = msg.pose.pose.position.z;
 
   // Compute distance
-  distance = sqrt((pow((x1 - x0), 2) + pow((y1 - y0), 2) + pow((z1 - z0), 2)));
+  distance = sqrt(pow((x1 - x0), 2) +
+                  pow((y1 - y0), 2) +
+                  pow((z1 - z0), 2));
 
-  // pub_Signal();
   pub_TF(msg);
 
   if (distance > dis_th)
@@ -148,15 +201,19 @@ void LIO_Pub::OdomCallback(nav_msgs::Odometry msg)
     z0 = z1;
 
     signal_num += 1;
+
     Signal = true;
     pub_Signal();
     Signal = false;
-    distance = 0;
 
-    pcl::PointCloud<pcl::PointXYZI>::Ptr registeredCloudBody(new pcl::PointCloud<pcl::PointXYZI>);
+    distance = 0.0;
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr registeredCloudBody(
+        new pcl::PointCloud<pcl::PointXYZI>);
 
     Eigen::Isometry3d transform = odom2isometry(msg);
     Eigen::Matrix4d transformMatrix = transform.inverse().matrix();
+
     pcl::transformPointCloud(*registeredCloud, *registeredCloudBody, transformMatrix);
 
     pcl::VoxelGrid<pcl::PointXYZI> voxel;
@@ -167,25 +224,35 @@ void LIO_Pub::OdomCallback(nav_msgs::Odometry msg)
     sensor_msgs::PointCloud2 output;
     pcl::toROSMsg(*registeredCloudBody, output);
 
-    std::string tmpNameSpace;
-    if (!NameSpace.empty() && NameSpace[0] == '/')
+    std::string tmpNameSpace = NameSpace;
+    if (!tmpNameSpace.empty() && tmpNameSpace[0] == '/')
     {
-      tmpNameSpace = NameSpace.substr(1); // 从索引 1 开始截取
+      tmpNameSpace = tmpNameSpace.substr(1);
     }
+
+    output.header.stamp = msg.header.stamp;
     output.header.frame_id = tmpNameSpace + SensorName;
 
     // Publish submap
     dislam_msgs::SubMap submapMsg;
     submapMsg.keyframePC = output;
     submapMsg.pose = msg.pose;
+
     // 关键帧编号
     submapMsg.id = static_cast<uint32_t>(signal_num);
+
     // 机器人编号
-    submapMsg.robot_id = static_cast<uint8_t>(std::stoi(RobotID));
+    // 这里已经不用 std::stoi 了，避免 stoi 崩溃
+    submapMsg.robot_id = static_cast<uint8_t>(RobotID);
+
     subMapPublisher_.publish(submapMsg);
-    
-    
     pointCloudPublisher_.publish(output);
+
+    ROS_INFO("Publish submap: robot_id = %d, submap_id = %d, points = %lu",
+             RobotID,
+             signal_num,
+             registeredCloudBody->size());
+
     registeredCloud.reset(new pcl::PointCloud<pcl::PointXYZI>);
   }
 }
@@ -194,15 +261,18 @@ void LIO_Pub::PCCallback(sensor_msgs::PointCloud2 msg)
 {
   pcl::PCLPointCloud2 registeredCloudPCL;
   pcl_conversions::toPCL(msg, registeredCloudPCL);
-  pcl::PointCloud<pcl::PointXYZI>::Ptr localRegisteredCloud(new pcl::PointCloud<pcl::PointXYZI>);
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr localRegisteredCloud(
+      new pcl::PointCloud<pcl::PointXYZI>);
+
   pcl::fromPCLPointCloud2(registeredCloudPCL, *localRegisteredCloud);
 
   pcl::VoxelGrid<pcl::PointXYZI> voxel;
   voxel.setInputCloud(localRegisteredCloud);
   voxel.setLeafSize(0.2, 0.2, 0.2);
   voxel.filter(*localRegisteredCloud);
+
   *registeredCloud += *localRegisteredCloud;
-  // pointCloud.push_back(localRegisteredCloud);
 }
 
 void LIO_Pub::pub_PC(sensor_msgs::PointCloud2 msg)
@@ -212,7 +282,6 @@ void LIO_Pub::pub_PC(sensor_msgs::PointCloud2 msg)
 
 void LIO_Pub::pub_Signal()
 {
-  // ROS_INFO("No. %d signal", signal_num);
   std_msgs::Bool signal;
   signal.data = Signal;
   signalPublisher_.publish(signal);
@@ -221,19 +290,34 @@ void LIO_Pub::pub_Signal()
 void LIO_Pub::pub_TF(nav_msgs::Odometry msg)
 {
   static tf::TransformBroadcaster tf_broadcaster;
+
   tf::Transform transform;
   tf::Quaternion q;
-  transform.setOrigin(tf::Vector3(msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z));
-  q.setValue(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w);
+
+  transform.setOrigin(tf::Vector3(msg.pose.pose.position.x,
+                                  msg.pose.pose.position.y,
+                                  msg.pose.pose.position.z));
+
+  q.setValue(msg.pose.pose.orientation.x,
+             msg.pose.pose.orientation.y,
+             msg.pose.pose.orientation.z,
+             msg.pose.pose.orientation.w);
+
   transform.setRotation(q);
+
   usleep(100);
-  // tf_broadcaster.sendTransform(tf::StampedTransform(transform, msg.header.stamp, NameSpace + "/odom", NameSpace + SensorName));
-  std::string tmpNameSpace;
-  if (!NameSpace.empty() && NameSpace[0] == '/')
+
+  std::string tmpNameSpace = NameSpace;
+  if (!tmpNameSpace.empty() && tmpNameSpace[0] == '/')
   {
-    tmpNameSpace = NameSpace.substr(1); // 从索引 1 开始截取
+    tmpNameSpace = tmpNameSpace.substr(1);
   }
-  tf_broadcaster.sendTransform(tf::StampedTransform(transform, msg.header.stamp, tmpNameSpace + "/odom", tmpNameSpace + SensorName));
+
+  tf_broadcaster.sendTransform(
+      tf::StampedTransform(transform,
+                           msg.header.stamp,
+                           tmpNameSpace + "/odom",
+                           tmpNameSpace + SensorName));
 }
 
 Eigen::Isometry3d LIO_Pub::odom2isometry(const nav_msgs::Odometry odom_msg)
@@ -248,17 +332,23 @@ Eigen::Isometry3d LIO_Pub::odom2isometry(const nav_msgs::Odometry odom_msg)
   quat.z() = orientation.z;
 
   Eigen::Isometry3d isometry = Eigen::Isometry3d::Identity();
+
   isometry.linear() = quat.toRotationMatrix();
   isometry.translation() = Eigen::Vector3d(position.x, position.y, position.z);
+
   return isometry;
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "LIO_Publisher");
+
   ros::NodeHandle n("~");
+
   LIO_Pub LIO_Pub(n);
+
   ROS_INFO("Init well!");
+
   ros::spin();
 
   return 0;
